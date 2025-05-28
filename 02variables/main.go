8199fdf388ebb4a0a5d143e3bfc8da8f4755211b
kaa-dan <nithinraj.dev@gmail.com>
2025-05-28 22:27:42 +0530

    variables

---
+++ 02variables/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+const LoginToken string = "gghdd" // public variable starts with capital leter
+
+func main() {
+	var username string = "nithin raj"
+
+	fmt.Println(username)
+
+	fmt.Printf("Variable is of type : %T \n", username)
+
+	//boolean
+
+	var isLoggedInd bool = false
+
+	fmt.Println(isLoggedInd)
+
+	fmt.Printf("Variable is of type : %T \n", isLoggedInd)
+
+	// uint
+
+	var smallvalue uint8 = 255
+
+	fmt.Println(smallvalue)
+
+	fmt.Printf("Variable is of type : %T \n", smallvalue)
+
+	//default values and some aliases
+	var anothervalue int
+
+	fmt.Println(anothervalue)
+
+	fmt.Printf("Variable is of type : %T \n", anothervalue)
+
+	// implicit type
+
+	var websit = "nithin.com"
+
+	fmt.Println(websit)
+
+	// no var style
+
+	numberOfUser := 3000
+
+	fmt.Println(numberOfUser)
+}
