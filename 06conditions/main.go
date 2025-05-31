92abc32a8ca0929f40947fe924a8d1adb40280bc
kaa-dan <nithinraj.dev@gmail.com>
2025-05-31 12:09:47 +0530

    conditions

---
+++ 06conditions/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"runtime"
+	"time"
+)
+
+func main() {
+
+	if v := math.Pow(1, 2); v < 10 {
+		fmt.Println(v)
+	} else {
+		fmt.Println("Kannas kadalas")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS x.")
+	default:
+		fmt.Printf("%s.\n", os)
+	}
+
+	today := time.Now().Weekday()
+
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today")
+	case today + 1:
+		fmt.Println("Tommorow")
+	case today + 2:
+		fmt.Println("In two days")
+	default:
+		fmt.Println("to far away")
+	}
+
+}
